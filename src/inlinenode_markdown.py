fe3e5fa34d3cd04a41c13891c606f3aadf099ac1
phpires <ph_pires@poli.ufrj.br>
2025-04-14 12:23:01 -0300

    extracting markdown images and link from text

--- src/inlinenode_markdown.py
+++ src/inlinenode_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 def split_nodes_delimiter(old_nodes, delimeter, text_type):
     new_nodes = []
@@ -13,3 +14,11 @@ def split_nodes_delimiter(old_nodes, delimeter, text_type):
             elif i % 2 != 0:
                 new_nodes.append(TextNode(splitted_nodes[i], text_type))
     return new_nodes
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
--- src/test_inlinenode_markdown.py
+++ src/test_inlinenode_markdown.py
@@ -1,8 +1,8 @@
 import unittest
 from textnode import TextNode, TextType
-from delimetersplitter import split_nodes_delimiter
+from inlinenode_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
-class TestDelimeterSplitter(unittest.TestCase):
+class TestInlineNodeMarkdown(unittest.TestCase):
     def test_delimeter_code_code_block(self):
         old_node = TextNode("This is text with a `code block` word", TextType.TEXT)
         delimeter = "`"
@@ -103,4 +103,33 @@ class TestDelimeterSplitter(unittest.TestCase):
             ],
             new_nodes
         )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+    
+    def test_extract_markdown_multiple_images(self):
+        matches = extract_markdown_images(
+            text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual([
+            ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+            ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")
+            ], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev")], matches)
+    
+    def test_extract_multiple_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual([
+            ("to boot dev", "https://www.boot.dev"),
+            ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
 
