4a7cb36c26a788613e72ff3e1021c26c7d10d813
phpires <ph_pires@poli.ufrj.br>
2025-04-17 11:12:37 -0300

    Regex to find type of block

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,18 +1,26 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = " ",
+    HEADING = r"^(#{1,6})\s+(.+)$", #OK
+    CODE = r'^```(?:\w+)?\s*\n(.*?)(?=^```)```', #ok
+    QUOTE = r"^>\s*(.+)$", #ok
+    UNORDERED_LIST = r"^\s*[-+*]\s+(.+)$",
+    ORDERED_LIST = r"^\s*\d+\.\s+(.+)$"
+
+
 def markdown_to_blocks(markdown):
-    blocks = markdown.split("\n\n")
-    blocks = list(map(lambda s : s.strip(), blocks))
-    blocks = list(filter(lambda s : s != "", blocks))
-    return blocks
+    splitted_blocks = markdown.split("\n\n")
+    stripped_blocks = list(map(lambda s : s.strip(), splitted_blocks))
+    return list(filter(lambda s : s != "", stripped_blocks))
 
-
-markdown = """
-This is **bolded** paragraph
-
-This is another paragraph with _italic_ text and `code` here
-This is the same paragraph on a new line
-
-
-
-- This is a list
-- with items
-"""
+def block_to_block_type(block):
+    for block_type in BlockType:
+        if block_type == BlockType.PARAGRAPH:
+            continue
+        pattern = block_type.value[0]
+        block_str = re.findall(pattern, block, re.DOTALL | re.MULTILINE)
+        if block_str:
+            return block_type
+    return BlockType.PARAGRAPH
