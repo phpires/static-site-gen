94bc1a8e140d0454d0f43b2a57a2732957e335af
phpires <ph_pires@poli.ufrj.br>
2025-04-22 15:02:23 -0300

    building website to docs

--- src/generate_page.py
+++ src/generate_page.py
@@ -11,7 +11,7 @@ def extract_title(markdown):
             return block.lstrip("#").strip()
     raise Exception("Not a h1 header.")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as md_file:
         md_content = md_file.read()
@@ -24,6 +24,7 @@ def generate_page(from_path, template_path, dest_path):
     html_content = markdown_to_html_node(md_content).to_html()
     h1_title = extract_title(md_content)
     page_html = template_html_content.replace("{{ Title }}", h1_title).replace("{{ Content }}", html_content)
+    page_html = page_html.replace("href=\"/", f"href=\"{basepath}").replace("src=\"/", f"src=\"{basepath}")
     
     if not os.path.exists(os.path.dirname(dest_path)):
         print(f"Destiny path not found. Creating: {dest_path}")
@@ -33,14 +34,14 @@ def generate_page(from_path, template_path, dest_path):
         print(f"Creating html to {dest_path}")
         f.write(page_html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     content_dir = os.listdir(dir_path_content)
     print(f"Listing content on {dir_path_content}: {content_dir}")
     for content in content_dir:
         content_path = os.path.join(dir_path_content, content)
         print(f"Analyzing content on {content_path}")
         if os.path.isfile(content_path):
-            generate_page(content_path, template_path, os.path.join(dest_dir_path, "index.html"))
+            generate_page(content_path, template_path, os.path.join(dest_dir_path, "index.html"), basepath)
         else:
             new_dest_dir_path = os.path.join(dest_dir_path, content)
-            generate_pages_recursive(content_path, template_path, new_dest_dir_path)
+            generate_pages_recursive(content_path, template_path, new_dest_dir_path, basepath)
--- src/main.py
+++ src/main.py
@@ -4,19 +4,21 @@ from generate_page import generate_pages_recursive
 
 import os
 import shutil
+import sys 
 
-public_path = "./public"
 static_path = "./static"
-
-content_index_md_path = "./content"
+content_path = "./content"
 template_html_path = "./template.html"
-public_index_html_path = "./public"
+doc_path = "./docs"
 
 def main():
-    print("Deleting public dir")
-    if os.path.exists(public_path):
-        shutil.rmtree(public_path)
-    copy_from_static_to_public(static_path, public_path)
-    generate_pages_recursive(content_index_md_path, template_html_path, public_index_html_path)
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    print("Deleting doc dir")
+    if os.path.exists(doc_path):
+        shutil.rmtree(doc_path)
+    copy_from_static_to_public(static_path, doc_path)
+    generate_pages_recursive(content_path, template_html_path, doc_path, basepath)
 
 main()
